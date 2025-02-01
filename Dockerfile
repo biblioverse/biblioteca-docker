f81799a407586b9102d0f9b16af6b75d66153840
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2025-02-01 22:10:30 +0100

    Only install extensions in the script

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,30 @@ ENV COMPOSER_HOME=/home/.composer
 RUN mkdir -p /home/.composer
 RUN printf "deb http://http.us.debian.org/debian stable main contrib non-free" > /etc/apt/sources.list.d/nonfree.list
 
+# npm is included in nodejs, see https://askubuntu.com/a/1432138
+RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y \
+    apt-transport-https \
+    build-essential \
+    ca-certificates \
+    cron \
+    curl \
+    dma  \
+    ghostscript \
+    git \
+    mariadb-client \
+    nodejs \
+    openssl \
+    p7zip-full \
+    p7zip-rar \ 
+    sudo \
+    supervisor \
+    unrar \
+    unzip \
+    vim \
+    wget  \
+    zip \
+    && rm -rf /var/lib/apt/lists/*
+
 # auto install dependencies and remove libs after installing ext: https://github.com/mlocati/docker-php-extension-installer
 COPY --from=mlocati/php-extension-installer /usr/bin/install-php-extensions /usr/local/bin/
 
--- docker/install.sh
+++ docker/install.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/bash
 
-# npm is included in nodejs, see https://askubuntu.com/a/1432138
-PACKAGES="apt-transport-https ca-certificates cron dma ghostscript mariadb-client nodejs openssl supervisor"
-PACKAGES_DEV="curl git sudo vim wget"
-PACKAGES_COMPRESS="p7zip-full p7zip-rar unrar unzip zip"
-
 install_php_extension() {
     local extension=$1
     local output
 
-    echo "Installing PHP extension $extension"
+    echo "[$(date +"%H:%M:%S")][$extension] Installing PHP extension"
 
     output=$(docker-php-ext-install "$extension" 2>&1)
     if [ $? -ne 0 ]; then
-        echo "Failed to install PHP extension $extension:"
+        echo "[$(date +"%H:%M:%S")][$extension] Failed to install PHP extension"
         echo "$output"
-        echo "Failed to install $extension"
+        echo "[$(date +"%H:%M:%S")][$extension] Failed to install extension"
         exit
     fi
 
-    echo "[ OK ] PHP extension $extension installed"
+    echo "[$(date +"%H:%M:%S")][$extension] PHP extension $extension installed"
 }
-
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update
-
-apt-get install -y ${PACKAGES} ${PACKAGES_DEV} ${PACKAGES_COMPRESS} ${PACKAGES_PHP} ${PACKAGES_PHP_VOLATILE}
 
 install_php_extension opcache
 install_php_extension pdo_mysql
@@ -36,5 +25,3 @@ install_php_extension exif
 install_php_extension gd
 install_php_extension intl
 install_php_extension @composer
-
-rm -rf /var/lib/apt/lists/*
